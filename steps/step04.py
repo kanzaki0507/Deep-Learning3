8283f48e74b842b4cdca5f61b9bf35f06d83dab6
kanzaki <kanzkai@MacPro.local>
2020-06-28 00:16:49 +0900

    [forward]

---
+++ steps/step04.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+class Function:
+    def __call__(self, input):
+        x = input.data # データを取り出す
+        y = self.forward(x) # 実際の計算
+        output = Variable(y) # Variableとして返す
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+def numerical_diff(f, x, eps=1e-4): # f:インスタンス変数
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+    return C(B(A(x)))
+
+x = Variable(np.array(0.5))
+dy = numerical_diff(f, x)
+print(dy)
